9b2870747975568831254eb6dda085664638c677
Benas Bitauskas <benasbitauskas@gmail.com>
2023-03-02 16:43:54 +0200

    pataisymai su main

---
+++ logs.py
@@ -0,0 +1,14 @@
+import logging
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler('methods.log')
+logger.addHandler(file_handler)
+
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
+file_handler.setFormatter(formatter)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
