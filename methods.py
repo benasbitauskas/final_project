431e439fd689e2bd9613d8cd3cdd05c04fe57d9c
Benas Bitauskas <benasbitauskas@gmail.com>
2023-03-05 19:17:45 +0200

    math solved

--- methods.py
+++ methods.py
@@ -35,7 +35,8 @@ def calculate_commercial_volume(new_dictionary):
 
 
 def sum_lvolume_total(calculated_lvolume):
-    return sum(calculated_lvolume.values())
+    lvolume_sum = sum(calculated_lvolume.values())
+    return lvolume_sum
 
 
 def timber_price(average_price, preparation_price):
@@ -43,30 +44,29 @@ def timber_price(average_price, preparation_price):
     return t_price
 
 
-def calculate_single_compensation():
-    single_compensation = round(sum_list[0] * t_price, 2)
+def calculate_single_compensation(t_price, lvolume_sum):
+    single_compensation = round(lvolume_sum * t_price, 2)
     return single_compensation
 
 
-# TODO sum_list[0] pakeisti i kintamaji
-
-
-def calculate_annual_compensation(interest):
+def calculate_annual_compensation(lvolume_sum, t_price, interest):
     if interest > 0:
-        annual_compensation = round(sum_list[0] * t_price * interest / 100, 2)
+        annual_compensation = round(lvolume_sum * t_price * interest / 100, 2)
         return annual_compensation
     else:
-        print(f'Kompensacija nemokama')
+        return f'Kompensacija nemokama'
 
 
-# TODO sum_list[0] pakeisti i kintamaji
 tree_volume_list = []
 tree_commercial_volume_list = []
 test_list = [{'P': 100}, {'P': 100}, {'B': 100}, {'D': 100}]
 new_dict = {'P': 200, 'B': 100, 'D': 100}
-average_price = 70, 69
+average_price = 70.69
 preparation_price = 13.90
 lvolume = {'P': 174.0, 'B': 83.0, 'D': 87.0}
+lvolume_total = 344
+t_price = 56.79
+interest = 2.06
 print(add_tree_volume('P', 100))
 print(add_tree_volume('P', 100))
 print(add_tree_volume('B', 100))
@@ -75,3 +75,6 @@ print(reduce_tree_volume_list(test_list))
 print(sum_volume_total(new_dict))
 print(calculate_commercial_volume(new_dict))
 print(sum_lvolume_total(lvolume))
+print(timber_price(average_price, preparation_price))
+print(calculate_single_compensation(t_price, lvolume_total))
+print(calculate_annual_compensation(t_price, lvolume_total, interest))
