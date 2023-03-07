3ee092945a220e5124334caad5cbb669080199cb
Benas Bitauskas <benasbitauskas@gmail.com>
2023-03-07 21:42:54 +0200

    unittest building

--- Test/test_methods.py
+++ Test/test_methods.py
@@ -1,23 +1,47 @@
 import unittest
-from methods import TreeSppVolume
+from methods import *
 
 
 class TestProblems(unittest.TestCase):
 
-    def setUp(self):
-        self.trees_vol1 = TreeSppVolume('P', 100)
-        self.trees_vol2 = TreeSppVolume('D', 60.5)
-        self.trees_vol3 = TreeSppVolume('B', -80)
-        self.trees_vol4 = TreeSppVolume('X', 80)
-
-    def test_calculate_lvolume(self):
-        self.assertEqual(self.trees_vol1._calculate_lvolume(), 87.0)
-        self.assertEqual(self.trees_vol2._calculate_lvolume(), 52.635)
+    def test_capitalize_input_tree_spp(self):
+        self.assertEqual('P', capitalize_input_tree_spp('p'))
+        self.assertEqual('BT', capitalize_input_tree_spp('bt'))
+        self.assertEqual('PJ', capitalize_input_tree_spp('pJ'))
         with self.assertRaises(ValueError):
-            self.trees_vol3._calculate_lvolume()
-        with self.assertRaises(KeyError):
-            self.trees_vol4._calculate_lvolume()
+            capitalize_input_tree_spp(10)
+        with self.assertRaises(ValueError):
+            capitalize_input_tree_spp(7.8)
 
-    def test_sum_lvolume_total(self):
-        self.assertEqual(self.trees_vol1._sum_lvolume_total(), 139.63)
-        self.assertEqual(self.trees_vol4._sum_lvolume_total(), 139.63)
+    def test_add_tree_volume(self):
+        self.assertEqual([{'P': 100}], add_tree_volume('P', 100))
+        self.assertEqual([{'P': 100}, {'B': 80}], add_tree_volume('B', 80))
+        self.assertEqual([{'P': 100}, {'B': 80}, {'P': 100}], add_tree_volume('P', 100))
+        with self.assertRaises(ValueError):
+            add_tree_volume('B', -80)
+        with self.assertRaises(KeyError):
+            add_tree_volume('X', 80)
+
+    def test_reduce_tree_volume_list(self):
+        self.assertEqual({'B': 80, 'P': 200}, reduce_tree_volume_list())
+
+
+# test_list = [{'P': 100}, {'P': 100}, {'B': 100}, {'D': 100}]
+# new_dict = {'P': 200, 'B': 100, 'D': 100}
+# average_price = 70.69
+# preparation_price = 13.90
+# lvolume = {'P': 174.0, 'B': 83.0, 'D': 87.0}
+# lvolume_total = 344
+# t_price = 56.79
+# interest = 2.06
+# print(add_tree_volume('P', 100))
+# print(add_tree_volume('P', 100))
+# print(add_tree_volume('B', 100))
+# print(add_tree_volume('D', 100))
+# print(f'2: {reduce_tree_volume_list()}')
+# print(f'3: {sum_volume_total()}')
+# print(f'4: {calculate_commercial_volume()}')
+# print(f'5: {sum_cvolume_total()}')
+# print(f'6: {calculate_timber_price(average_price, preparation_price)}')
+# print(f'7: {calculate_single_compensation()}')
+# print(f'8: {calculate_annual_compensation(interest)}')
