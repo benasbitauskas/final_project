c92f19c3c8247ee1b102ffc372c2896944840672
Benas Bitauskas <benasbitauskas@gmail.com>
2023-03-10 15:47:26 +0200

    database build fixing

--- structure.py
+++ structure.py
@@ -1,9 +1,8 @@
 import datetime
-from sqlalchemy import Column, Integer, String, Float, DateTime, create_engine
+from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime
 from sqlalchemy.orm import declarative_base
 
-
-engine = create_engine('sqlite:///compensation.db')
+engine = create_engine('sqlite:///compensation.db', echo=True)
 Base = declarative_base()
 
 
@@ -12,23 +11,23 @@ class Info(Base):
     id = Column(Integer, primary_key=True)
     tree_volume = Column('2', String)
     t_price = Column('7', Float)
-    single = Column('8', Float)
-    annual = Column('9', Float)
+    single_compensation = Column('8', Float)
+    annual_compensation = Column('9', Float)
     create_date = Column('Created_date', DateTime, default=datetime.datetime.utcnow)
 
     # TODO FIX
     def __init__(self, tree_volume, t_price, single_compensation, annual_compensation):
-        self.tree_volume_list = tree_volume
+        self.tree_volume = tree_volume
         self.t_price = t_price
         self.single_compensation = single_compensation
         self.annual_compensation = annual_compensation
 
-    # def __repr__(self):
-    #     return (
-    #         f'No.: {self.id}| Plot_id: {self.plot_id}| Commercial volume: {self.sum_vol}| '
-    #         f'Annual compensation: {self.annual}| Single compensation: {self.single}|'
-    #         f'Created date: {self.create_date}'
-    #     )
+    def __repr__(self):
+        return (
+            f'No.: {self.id}| 2: {self.tree_volume}| 7: {self.t_price}| '
+            f'8: {self.single_compensation}| 9: {self.annual_compensation}|'
+            f'Created date: {self.create_date}'
+        )
 
 
-Base.metadata.create_all(engine)
+Base.metadata.create_all(bind=engine)
